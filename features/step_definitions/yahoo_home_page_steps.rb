3da180eb0b904d0fa59387548cc9126bccfc3a83
IgorN <igor.nikityuk@limos.com>
2015-03-20 10:53:56 +0200

    Set to_i

--- features/step_definitions/yahoo_home_page_steps.rb
+++ features/step_definitions/yahoo_home_page_steps.rb
@@ -19,6 +19,6 @@ Then(/^page should load in "(.*?)" seconds$/) do |second_count|
   @end_page_load_time = Time.now
   #get the difference between times
   difference = (@end_page_load_time - @start_page_load_time).to_f
-  expect(difference).to be < second_count
+  expect(difference).to be < second_count.to_i
   puts 'Loading page took ' + difference.to_s + ' seconds'
 end
